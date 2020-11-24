0870872a0208c8efc07cfb0c9a4d8a8fce2eccb6
mikikomikiko <72443008+mikikomikiko@users.noreply.github.com>
2020-11-24 12:19:06 +0100

    20201124 miki

--- Group project draft.R
+++ Group project draft.R
@@ -1,5 +1,14 @@
 
 
-Data <- read.table("Data for project.csv", dec = ",", sep = ";")
+Data <- read.table("Data_for_project.csv", dec = ",", sep = ";")
 mean(Data_for_project$aphidsinoculated_init)
+
+#Aphid population growth was modelled using a linear mixed model
+install.packages("nlme")
+#Parasitism rates (the ratio of parasitized to all aphids) 
+#and syrphid fractions (the ratio of syrphids to total aphids + syrphids) 
+#were modelled using a binomial response with logit link
+install.packages("lme4")
+#using sequential AICc testing with the function “dredge” 
+install.packages("MuMIn")
 
