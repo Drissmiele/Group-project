e26c0c75a6e39ba96dd7144ecd3997ce44dad98f
Drissmiele <adriele.benedetto@gmail.com>
2020-12-01 13:17:31 +0100

    Notes for github

--- Notes for github.R
+++ Notes for github.R
@@ -1,4 +1,4 @@
 # Notes for git
 # 1. If git is empty, you can pull to verify if already is to date.
-# 2. Make
-# 3. Right click --> revert the file -->
+# 2. Make sure you open a new file every time you practice and save it with  specific title
+# 3. find the doc with problem --> copy it somewhere safe --> Right click --> revert the file that is causing problem --> get the version of the doc that works --> and then push it
