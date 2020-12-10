2fe36705cc8eca01945aff806504bba1d12426dc
Drissmiele <adriele.benedetto@gmail.com>
2020-12-10 18:08:10 +0100

    syrphid fraction calculation

---
+++ Syrphid fraction calculation.R
@@ -0,0 +1,16 @@
+library(readr)
+Data1 <- read.table("Project data.csv", header = TRUE, dec = ",", sep =";")
+
+# created a new variable called plant_ID that is highly specific (plant number)
+Data1$plant_ID <- paste(Data1$Treatment, Data1$Plant, sep ="_")
+
+#selecting in Data 2 all the rows with buff = 100
+BUFF <- Data1[Data1$BUFF_DIST =="100",] 
+View(BUFF)
+
+# creating the syrphid fraction variable 
+
+syrphid_fraction <- (BUFF$syrphidl_p/(BUFF$aphid_live + BUFF$syrphidl_p))
+BUFF$syrphid_fraction <- syrphid_fraction
+
+
