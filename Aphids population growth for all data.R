c1ab67da820c6d13680ff07a201bf64845cfb93a
mikikomikiko <72443008+mikikomikiko@users.noreply.github.com>
2020-12-10 15:57:58 +0100

    Data frame Data1 + aphids population growth rate for 12000 data

--- Aphids population growth for all data.R
+++ Aphids population growth for all data.R
@@ -2,11 +2,13 @@
 library(readr)
 Data1 <- read.table("Project data.csv", header = TRUE, dec = ",", sep =";")
 
-#calculate the aphids population growth at date1
-
+#calculate the aphids population growth 
 logNaphids_all <- log(Data1$aphid_live+1) - log(Data1$aphidsinoculated_init+1) 
 APG_all <- logNaphids_all/10
 
-APG_all
+#view aphids population growth for 12000 data
 View(APG_all)
 
+#make new data frame which include aphids population growth rate
+Data1_APG_all <- data.frame(Data1, APG_all)
+View(Data1_APG_all)
