8cf5abbb931f19d5fff192a6f5cc210b66000aef
mikikomikiko <72443008+mikikomikiko@users.noreply.github.com>
2020-12-11 19:17:16 +0100

    B

--- Date1_aphids population growth and seminatural habitat.R
+++ Date1_aphids population growth and seminatural habitat.R
@@ -9,19 +9,11 @@ summary(DATE1)
 View(DATE1)
 
 #calculate the aphids population growth at date1
-logNaphids_D1 <- log(DATE1$aphid_live + 1) - log(DATE1$aphidsinoculated_init + 1) 
+logNaphids_D1 <- log(DATE1$aphid_live+1) - log(DATE1$aphidsinoculated_init+1) 
 APG_D1 <- logNaphids_D1/10
 
-<<<<<<< HEAD
 #dataframe for seminatural habitat and aphids population growth at date1 
 DATE1APG <- data.frame(DATE1,APG_D1)
-
-=======
-#data frame for seminatural habitat and aphids population growth at date1 
-SA_D1 <- data.frame(DATE1$Pt.seminatural, APG_D1)
-View(SA_D1)
->>>>>>> 13492120f2e8b6918406fd322069f6680f70c214
-
 
 #barplot for % Seminatural habitat and APhid population growth
 library(ggplot2)
@@ -29,14 +21,6 @@ q <- ggplot(DATE1APG, aes(x = Pt.seminatural...., y = APG_D1))
 q <- q  + geom_quantile(size=1)
 q <- q + labs(x= "% seminatural habitat", y="Aphids population growth")
 q
-
-#####reference
-q <- ggplot(data2,aes(DAY,NUMBER,colour=COUNTRY))
-q <- q + geom_line(size=1) 
-q <- q + labs(x="日付", y="感染者数[人]", colour="国")
-q <- q + scale_x_date(breaks = function(x) seq.Date(from=data2[1,1],to=max(x),by="4 days"),date_labels="%m月%d日")
-q
-
 
 #####################################
 #lmer for plot (not yet, trying)
