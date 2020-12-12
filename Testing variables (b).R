7e38bdfbbafb5f00e52414dc5692fcc365a56a74
Drissmiele <adriele.benedetto@gmail.com>
2020-12-12 13:12:39 +0100

    Lm(1-4)

--- Testing variables (b).R
+++ Testing variables (b).R
@@ -2,31 +2,44 @@ Data1 <- read.table("Project data.csv", header = TRUE, dec = ",", sep = ";")
 
 # Aphids parasitized - aphid density
 
-lm1 <- lm(Data1$aphid_live ~ Data1$aphid_parasitized)
-summary(lm1)
+lm2 <- lm(Data1$aphid_live ~ Data1$aphid_parasitized)
+summary(lm2)
 
 p <- ggplot(Data1, aes(x=aphid_parasitized,y=(aphid_live), color= aphid_parasitized)) +
   geom_point() + 
   geom_smooth(method="lm")
 print(p)
 
-# aphid parasitized - aphid pop growth 
 
-logNaphids_all <- log(Data1$aphid_live + 1) - log(Data1$aphidsinoculated_init + 1) 
-APG_all <- logNaphids_all/10
-Data1$APG_all <- APG_all
-
-lm2 <- lm(Data1$APG_all ~ Data1$aphid_parasitized)
-summary(lm2)
-
-p <- ggplot(Data1, aes(x=aphid_parasitized,y=(APG_all), color= aphid_parasitized)) +
+# aphid density - aphids parasitized
+lm3 <- lm(Data1$aphid_parasitized ~ Data1$aphid_live)
+summary(lm3)
+p <- ggplot(Data1,aes(x=aphid_live,y=(aphid_parasitized), color= aphid_live)) +
   geom_point() + 
   geom_smooth(method="lm")
 print(p)
 
+# syrphid fraction - aphid
 
-# aphid density - aphids parasitized 
-lm3 <- lm(Data1$aphid_parasitized ~ Data1$aphid_live)
-summary(lm3)
+syrphid_fraction <- (Data1$syrphidl_p /((Data1$aphid_live)+(Data1$syrphidl_p)))
+Data1$syrphid_fraction <- syrphid_fraction
+Data2 <- na.omit(Data1)
+# => lots of NaN, dataframe 2 has only half of the obs. 
 
+lm4 <- lm(Data1$aphid_live~Data1$syrphid_fraction)
+summary(lm4)
+p <- ggplot(Data1,aes(x=syrphid_fraction,y=(aphid_live), color= aphid_live)) +
+  geom_point() + 
+  geom_smooth(method="lm")
+print(p)
 
+#syrphid larvae - aphids
+
+lm4_2 <- lm(Data1$aphid_live ~Data1$syrphidl_p)
+summary(lm4_2)
+p <- ggplot(Data1,aes(x=syrphidl_p,y=(aphid_live), color= aphid_live)) +
+  geom_point() + 
+  geom_smooth(method="lm")
+print(p)
+
+# the effect of syrphid larvae on aphid is significant (not by chance)
