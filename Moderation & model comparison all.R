a987ad50ac8c7b0b402c8f55e1926d039a779559
Drissmiele <adriele.benedetto@gmail.com>
2020-12-12 14:10:32 +0100

    minor modification

--- Moderation & model comparison all.R
+++ Moderation & model comparison all.R
@@ -1,3 +1,5 @@
+Data1 <- read.table("Project data.csv", header = TRUE, dec = ",", sep = ";")
+
 #6 aphids and biomass
 lm6 <- lm(Data1$Biomass_fin ~ Data1$aphid_live)
 summary(lm6) 
@@ -27,13 +29,13 @@ plot(Data1$aphid_live ~ as.factor(Data1$Field_Mgmt), xlab = "Field Management", 
 
 # moderation effect: Field Management (conventional/organic) and aphids with a possible interaction with crop type
 unique(Data1$croptype)
-lm7a <- lm(Data1$aphid_live ~ as.numeric(as.factor(Data1$Field_Mgmt) + as.numeric(as.factor(Data1$croptype)) + I((as.numeric(as.factor(Data1$Field_Mgmt)))* (as.numeric(as.factor(Data1$croptype))))))
+lm7a <- lm(Data1$aphid_live ~ as.numeric(as.factor(Data1$Field_Mgmt)) + as.numeric(as.factor(Data1$croptype)) + I(as.numeric(as.factor(Data1$Field_Mgmt))* as.numeric(as.factor(Data1$croptype))))
 summary(lm7a)
 
 #model comparison
 AIC(lm7, lm7a)
-# lm7 is better because lower AIC values indicate a better-fit model. 
-# there is no moderation, the direct relationship is able to explain what is going on better
+# lm7a is better. 
+# there is a little bit of moderation.
 
 #8 Management and syruphids ll
 lm8 <- lm(Data1$syrphidl_p ~ Data1$Field_Mgmt)
@@ -41,3 +43,7 @@ summary(lm8)
 # p value =  <2e-16
 #Plot
 plot(Data1$syrphidl_p ~ as.factor(Data1$Field_Mgmt), ylab = "Syrphid", xlab = "Field Management")
+
+
+
+as.numeric(as.factor((Data1$Field_Mgmt)))
